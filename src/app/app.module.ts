00ddfc69bc39ae85f0915b5d8805b84d19f307a0
Shifin <shifin@localhost.localdomain>
2020-04-14 18:04:49 +0530

    checkin

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item.component';
+
+@NgModule({
+    imports:[
+        BrowserModule
+    ],
+    declarations:[
+        AppComponent,
+        MediaItemComponent
+    ],
+    bootstrap:[
+        AppComponent,
+        MediaItemComponent
+    ]
+})
+
+export class AppModule{}
