648ba482f78131d1ca3e1aa3a42d64d740dc60e6
Shifin <shifin@localhost.localdomain>
2020-04-18 22:37:21 +0530

    Directives and pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
+import { MediaItemListComponent } from './media-item-list.component';
+import { FavouriteDirective } from './favourite.directive';
+import { CategoryList } from './category-list.pipe';
 
 @NgModule({
     imports:[
@@ -9,7 +12,10 @@ import { MediaItemComponent } from './media-item.component';
     ],
     declarations:[
         AppComponent,
-        MediaItemComponent
+        MediaItemComponent,
+        MediaItemListComponent,
+        FavouriteDirective,
+        CategoryList
     ],
     bootstrap:[
         AppComponent
